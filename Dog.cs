48ee9f0ec2a5ffc555140d47b02e1dea0f63850d
Renu2811 <1243renuu@gmail.com>
2022-05-17 20:40:38 -0700

    program.cs(object file) Dog.cs(class file)

---
+++ Dog.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    public class Dog
+    {
+       public  string breed;
+
+        public void bark()
+        {
+            Console.WriteLine("Barking..");
+
+        }
+
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // create Dog object 
+            Dog bullDog = new Dog();
+
+            // access breed of the Dog 
+            bullDog.breed = "Bull Dog";
+            Console.WriteLine(bullDog.breed);
+
+            // access method of the Dog
+            bullDog.bark();
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
+        
