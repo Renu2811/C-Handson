c6aaac4c979b1fafeab8a9bf2230d488103c3bbd
Renu2811 <1243renuu@gmail.com>
2022-05-17 20:58:05 -0700

    Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject1
+{
+
+    //Creating multiple objects in one class
+    public class Employee
+    {
+        string department;
+
+        static void Main(string[] args)
+        {
+
+            // create Employee object 
+            Employee sheeran = new Employee();
+
+            // set department for sheeran
+            sheeran.department = "Development";
+            Console.WriteLine("Sheeran: " + sheeran.department);
+
+            // create second object of Employee
+            Employee taylor = new Employee();
+
+            // set department for taylor
+            taylor.department = "Content Writing";
+            Console.WriteLine("Taylor: " + taylor.department);
+
+            Console.ReadLine();
+        }
+    }
+}
