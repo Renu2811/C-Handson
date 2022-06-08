c7a21736f887cfc3c82c4bcf225f00ee728dfb92
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-06-08 04:19:08 -0700

    Add files via upload

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TaskParallelExecution taskParallelExecution = new TaskParallelExecution();
+
+
+            //taskParallelExecutions.DisplayCars();
+
+            //creating threads
+
+            Thread t1 = new Thread(taskParallelExecution.ShowBuyerReport);
+            Thread t2 = new Thread(taskParallelExecution.ShowDealerReport);
+
+            //stating threads
+            t1.Start();
+            t2.Start();
+            Console.ReadKey();
+
+        }
+    }
+}
+
+   
