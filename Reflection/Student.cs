3688f50d31521a097796511e9c22399a927c5062
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-06-08 04:24:10 -0700

    Add files via upload

---
+++ Reflection/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public  class Student
+    {
+        //properities defination
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        //constructor with no argument
+        public Student()
+        {
+            RollNo = 0;
+            Name = String.Empty;
+        }
+        //constructor with parameters
+        public Student(int rollno, string name)
+        {
+            RollNo = rollno;
+            Name = name;
+        }
+        //method to display student data
+        public void DisplayData()
+        {
+            Console.WriteLine("Roll No: {0}", RollNo);
+            Console.WriteLine("Name: {0}", Name);
+
+        }
+    }
+}
+    
