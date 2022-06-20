a43dd3ab0fdf584754bdb08521ffcf73815d0aee
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-06-19 20:14:15 -0700

    Add files via upload

---
+++ DLLFile/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EbBill;
+
+namespace DLLFile
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EBbill e = new EBbill();
+            e.CustomerData(230, "latha", 43);
+            e.CustomerData(231, "pavan", 7);
+            e.CustomerData(232, "karthik", 27);
+            e.CustomerData(233, "Manoj", 74);
+            e.CustomerData(234, "tanusri", 41);
+            e.CustomerData(235, "Bhavani", 64);
+            e.CustomerData(236, "Gowri", 15);
+            e.CustomerData(237, "Mahesh", 32);
+            e.CustomerData(238, "lakshmi", 35);
+        TOP:
+            Console.WriteLine("choose the option: \n 1 for Customer detalis \n 2 for show EBbill \n 3 for all bill report\n");
+            int i = Convert.ToInt32(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Enter customer id to view details");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        e.ShowCustomerDetails(id);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Enter customer id to view details");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        e.ShowCustomerDetails(id);
+                        break;
+                    }
+                case 3:
+                    {
+                        e.ShowEbBillReport();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("choose correct option");
+                        goto TOP;
+                    }
+            }
+            Console.Read();
+
+        }
+    }
+}
+
+        
---
+++ EbBill/EBbill.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace EbBill
+{
+    public class EBbill
+    {
+        public int customerID { get; set; }
+        public string customerName { get; set; }
+        public int noOfUnits { get; set; }
+        public int unitsPerCost { get; set; } = 10;
+        public int total { get; set; }
+
+        List<EBbill> ebill = new List<EBbill>();
+
+        private EBbill(int customerID, string customerName, int noOfUnits)
+        {
+            this.customerID = customerID;
+            this.customerName = customerName;
+            this.noOfUnits = noOfUnits;
+            tBill();
+        }
+        public EBbill() { }
+
+        public void CustomerData(int iD, String name, int units)
+        {
+            EBbill bill = new EBbill(iD, name, units);
+            ebill.Add(bill);
+        }
+        private void tBill()
+        {
+            total = unitsPerCost * noOfUnits;
+        }
+        public void ShowCustomerDetails(int customerID)
+        {
+            foreach (var data in ebill)
+            {
+                if (data.customerID == customerID)
+                    Console.WriteLine("CustomerID :" + data.customerID + "\tCustomerName :" + data.customerName + "\tUsage in Units :" + data.noOfUnits);
+            }
+        }
+        public void ShowEbBill(int customerID)
+        {
+            foreach (var data in ebill)
+            {
+                if (data.customerID == customerID)
+                    Console.WriteLine("CustomerID :" + data.customerID + "\tCustomerName :" + data.customerName + "\tUsage in Units :" + data.noOfUnits + "\tTotalBill :" + data.total);
+            }
+        }
+        public void ShowEbBillReport()
+        {
+            foreach (var data in ebill)
+            {
+                Console.WriteLine("CustomerID :" + data.customerID + "\tCustomerName :" + data.customerName + "\tUsage in Units :" + data.noOfUnits + "\tTotalBill :" + data.total);
+            }
+
+        }
+
+    }
+}
+    
