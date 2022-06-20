9f5aedcf1ae314edbe911022a236546f78961072
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-06-19 19:53:09 -0700

    Add files via upload

---
+++ Delegates/DelegateClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    delegate void message(string msg);
+    public  class DelegateClass
+    {
+        public void Hello(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public void Hii(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public void Gm(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+    }
+}
+
+
+  
