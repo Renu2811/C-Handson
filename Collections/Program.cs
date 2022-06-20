19aa6a5859bd22d4aec39bd8a9bfa6e3bca0e50a
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-06-19 20:27:20 -0700

    Add files via upload

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentGrades lg = new StudentGrades();
+            lg.gradebook();
+            lg.StudGrade();
+            Console.Read();
+        }
+    }
+}
+      
